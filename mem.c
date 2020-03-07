38afc578301dc41002fd8ec08f676982ffbde690
MintSoup <aregak2005@gmail.com>
2020-03-07 22:12:42 +0400

    Now returning pointer from dynamic allocation (no idea how this didn't break the code before, probably C implicit returns)

--- mem.c
+++ mem.c
@@ -3,10 +3,10 @@
 #include "commons.h"
 #include "mem.h"
 
-void* reallocate(void* previous, size_t oldSize, size_t newSize){
-    if(newSize == 0){
-        free(previous);
-        return NULL;
-    }
-    realloc(previous, newSize);
+void *reallocate(void *previous, size_t oldSize, size_t newSize) {
+	if (newSize == 0) {
+		free(previous);
+		return NULL;
+	}
+	return realloc(previous, newSize);
 }
